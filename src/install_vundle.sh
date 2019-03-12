87c210d585fd2d123b1c15aa5ed743fd20922dbf
Ruben Hagen <rubenhagen@skynet.local>
2019-03-12 10:29:21 +0100

    add function for symlinking .vimrc

--- src/install_vundle.sh
+++ src/install_vundle.sh
@@ -18,10 +18,16 @@ install_plugins()
 vim +PluginInstall +qall
 }
 
+#Symlink .vimrc
+symlink_dotfiles()
+{
+ln -s ../.vimrc ~/.vimrc
+}
+
 echo "This will install Vundle and plugins on your system. Do you want to continue?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) download_vundle; install_plugins; break;;
+        Yes ) download_vundle; install_plugins; symlink_dotfiles; break;;
         No ) exit;;
     esac
 done
